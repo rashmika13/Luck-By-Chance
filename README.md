fda8dc4a7afb1a8a3f7de27c33d418de883363c1
R <50966008+rashmika13@users.noreply.github.com>
2020-05-28 15:42:34 -0400

    final game

---
+++ README.md
@@ -0,0 +1,49 @@
+# LUCK BY CHANCE : A card matching game !
+
+<p style="font-family: times, serif; font-size:14pt; font-style:italic">This is a card game that I used to play during my childhood and I created an online version for my Project 1 of Software Engineering course. </p>
+
+[Click here to play the game]()
+
+# Game Instructions
+
+![Instructions Image](Gameimages/../Game%20images/Game%20Instructions.png)
+
+# Game Board
+
+![Game Board Image](Gameimages/../Game%20images/Game%20Board.png)
+
+# Game in play
+
+![Game in play Image](Gameimages/../Game%20images/Player%201%20Turn.png)
+
+## Player 1 wins
+
+![Player 1 wins Image](Gameimages/../Game%20images/Player%201%20Wins.png)
+
+## Player 2 wins
+
+![Player 2 wins Image](Gameimages/../Game%20images/Player%202%20Wins.png)
+
+## Game tie
+
+![Game tie Image](Gameimages/../Game%20images/Tie.png)
+
+# Technologies used
+
+- HTML
+- CSS
+- JavaScript
+
+## How to Play
+
+- Shuffle deck to start
+- When indicated each player takes turn to flip a card from the deck
+- To play again click shuffle deck
+
+## How to Win
+
+When the active player has a matching card value ( value K = value K ) with the opponent the active player wins !
+
+<p style="font-size:15pt;font-style:italic">Moving forward...</p>
+
+I would like to add more features to the game such as- each time the card value matches for the active player , all cards played thus far get added to its deck and the game continues until there are no cards left in one of the decks.
--- main.js
+++ main.js
@@ -1,167 +1,188 @@
 //constants
-const suits = ['h', 'c', 'd', 's'];
-const values  = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13'];
+const suits = ["h", "c", "d", "s"];
+const values = [
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+];
 
 let deck = [];
 
 for (suit in suits) {
-    for (value in values) {
-        deck.push(`${suits[suit]}${values[value]}`) 
-    }
+  for (value in values) {
+    deck.push(`${suits[suit]}${values[value]}`);
+  }
 }
-// Game state 
+// Game state
 let playerOneDeck = [];
 let playerOnePlayed;
 let playerTwoDeck = [];
 let playerTwoPlayed;
-let turn 
-let winner
+let turn;
+let winner;
 
-// cached elements
-const shuffleBtn = document.querySelector('#shuffle')
-const activeCardOneEl = document.getElementById('activeCardOne')
-const activeCardTwoEl = document.getElementById('activeCardTwo')
-const playerOneDeckEl = document.getElementById('playerOneDeck')
-const playerTwoDeckEl = document.getElementById('playerTwoDeck')
-const playerOneWinEl = document.getElementById('win1')
-const playerTwoWinEl = document.getElementById('win2')
-const itsATieEl = document.getElementById('tie')
-const playerTurnEl = document.getElementById('playerturn')
-const gameInstructionsEl = document.getElementById('game-instructions')
-const enterButtonEl = document.getElementById('enterbtn')
+/// cached elements
+const shuffleBtn = document.querySelector("#shuffle"); // Shuffle button-shuffles deck
+// Player decks and active cards
+const playerOneDeckEl = document.getElementById("playerOneDeck");
+const activeCardOneEl = document.getElementById("activeCardOne");
+const activeCardTwoEl = document.getElementById("activeCardTwo");
+const playerTwoDeckEl = document.getElementById("playerTwoDeck");
+// win or tie
+const playerOneWinEl = document.getElementById("win1");
+const playerTwoWinEl = document.getElementById("win2");
+const itsATieEl = document.getElementById("tie");
+
+// Turn
+const playerTurnEl = document.getElementById("playerturn");
+
+// Game menu
+const gameMenuEl = document.querySelector(".game-menu");
+const gameInstructionsEl = document.getElementById("game-instructions");
+const enterButtonEl = document.getElementById("enterbtn");
 
 // Event Listeners
-shuffleBtn.addEventListener('click', initialize)
-playerOneDeckEl.addEventListener('click', takeTurn)
-playerTwoDeckEl.addEventListener('click', takeTurnTwo)
-enterButtonEl.addEventListener('click', enterGame)
+shuffleBtn.addEventListener("click", initialize);
+playerOneDeckEl.addEventListener("click", takeTurn);
+playerTwoDeckEl.addEventListener("click", takeTurnTwo);
+enterButtonEl.addEventListener("click", enterGame);
 
 // Functions
+
+//Enter game to initialize the game board
 function enterGame() {
-    gameInstructionsEl.style.display = "none"
+  gameInstructionsEl.style.display = "none";
+  gameMenuEl.classList.remove("hide");
+  initialize();
 }
-
-
-
-initialize()
-
+// Shuffles deck , sets two deck with 26 cards each, starts with player one turn
 function initialize() {
-    shuffleDeck()
-    playerOneDeck = deck.slice(0, 26)
-    playerOnePlayed = null
-    playerTwoDeck = deck.slice(26, 52)
-    playerTwoPlayed = null
-    turn = 'One'
-    winner = null
-    render()
+  shuffleDeck();
+  playerOneDeck = deck.slice(0, 26);
+  playerOnePlayed = null;
+  playerTwoDeck = deck.slice(26, 52);
+  playerTwoPlayed = null;
+  turn = "One";
+  winner = null;
+  render();
 }
+
+// shuffles deck , resets deck and highlights turn
 function shuffleDeck() {
-let  i = 0
-     j = 0
-    temp = null
+  let i = 0;
+  j = 0;
+  temp = null;
   for (i = deck.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1))
-    temp = deck[i]
-    deck[i] = deck[j]
-    deck[j] = temp
+    j = Math.floor(Math.random() * (i + 1));
+    temp = deck[i];
+    deck[i] = deck[j];
+    deck[j] = temp;
   }
-  if (winner === 'One'|| winner === 'Two' || winner === 'tie') {
-    shuffleBtn.style.backgroundColor = "#4CAF50"
-    playerOneWinEl.innerHTML= ""
-    playerTwoWinEl.innerHTML= ""
-    itsATieEl.innerText = ""
-    playerOnePlayed = ""
-    playerTwoPlayed = ""
-    winner = ""
-    turn = "One"
-    
-  }  
-  highlightPlayerTurn()
+  if (winner === "One" || winner === "Two" || winner === "tie") {
+    shuffleBtn.style.backgroundColor = "#4CAF50";
+    playerOneWinEl.innerHTML = "";
+    playerTwoWinEl.innerHTML = "";
+    itsATieEl.innerText = "";
+    playerOnePlayed = "";
+    playerTwoPlayed = "";
+    winner = "";
+    turn = "One";
+  }
+  highlightPlayerTurn();
 }
 
-
+//compares values of cards played
 function cardsAreEqual() {
-    let valOne = null
-    let valTwo = null
-    if (playerOnePlayed) {
-        valOne = playerOnePlayed.slice(-2)
-    }
-    if (playerTwoPlayed) {
-        valTwo = playerTwoPlayed.slice(-2)
-    }
-    return valOne === valTwo
+  let valOne = null;
+  let valTwo = null;
+  if (playerOnePlayed) {
+    valOne = playerOnePlayed.slice(-2);
+  }
+  if (playerTwoPlayed) {
+    valTwo = playerTwoPlayed.slice(-2);
+  }
+  return valOne === valTwo;
 }
 
-
+//highlights active player's deck
 function highlightPlayerTurn() {
-    if (turn === '' || winner) {
-        playerOneDeckEl.style.border = ""
-        playerTwoDeckEl.style.border = ""
-        playerTurnEl.innerText= ""
-    } else if (turn === 'One'){
-        playerTwoDeckEl.style.border = ""
-        playerOneDeckEl.style.border = "5px solid red"
-        playerTurnEl.innerText= "Player One's Turn"
-    } else if (turn === 'Two'){
-        playerOneDeckEl.style.border = ""
-        playerTwoDeckEl.style.border = "5px solid red"
-        playerTurnEl.innerText= "Player Two's Turn"
-    } else {
-        playerOneDeckEl.style.border = ""
-        playerTwoDeckEl.style.border = ""
-        playerTurnEl.innerText= ""
-    }
-    }
-
-
-function takeTurn() {
-    if (turn === 'Two' || winner) return
-    playerOnePlayed = playerOneDeck.shift() 
-    if (cardsAreEqual()) {
-        playerOneWinEl.innerHTML= "Player 1 Wins !" ;
-        winner = 'One'
-    }
-    turn = 'Two'
-    highlightPlayerTurn()
-    render()
-   
-} 
-
-function takeTurnTwo () {
-    if (turn === 'One' || winner) return
-    playerTwoPlayed = playerTwoDeck.shift()
-    if (cardsAreEqual()) {
-        playerTwoWinEl.innerText= "Player 2 Wins !"
-        winner = 'Two'
-    }
-    if (playerTwoDeck.length === 0) {
-        winner = 'Tie'
-    }
-    turn = 'One'
-    highlightPlayerTurn()
-    render ()
-} 
-
-function render() {
-    activeCardOneEl.className = 'card xlarge'
-    activeCardTwoEl.className = 'card xlarge'
-  
-    if (playerOnePlayed) {
-      activeCardOneEl.classList.add(playerOnePlayed)
-      activeCardOneEl.textContent = playerOnePlayed
-    }
-  
-    if (playerTwoPlayed) {
-      activeCardTwoEl.classList.add(playerTwoPlayed)
-      activeCardTwoEl.textContent = playerTwoPlayed
-    }
-
-    if (winner) {
-        if (winner === 'Tie') {
-            itsATieEl.innerText = "Its a tie !"
-        } else {
-            shuffleBtn.style.backgroundColor = "red"
-        }
-    } 
+  if (turn === "" || winner) {
+    playerOneDeckEl.style.border = "";
+    playerTwoDeckEl.style.border = "";
+    playerTurnEl.innerText = "";
+  } else if (turn === "One") {
+    playerTwoDeckEl.style.border = "";
+    playerOneDeckEl.style.border = "5px solid red";
+    playerTurnEl.innerText = "Player One's Turn";
+  } else if (turn === "Two") {
+    playerOneDeckEl.style.border = "";
+    playerTwoDeckEl.style.border = "5px solid red";
+    playerTurnEl.innerText = "Player Two's Turn";
+  } else {
+    playerOneDeckEl.style.border = "";
+    playerTwoDeckEl.style.border = "";
+    playerTurnEl.innerText = "";
+  }
 }
 
+// determines player 1 win  or tie ; switches turn; highlights active card
+function takeTurn() {
+  if (turn === "Two" || winner) return;
+  playerOnePlayed = playerOneDeck.shift();
+  if (cardsAreEqual()) {
+    playerOneWinEl.innerHTML = "Player 1 Wins !";
+    winner = "One";
+  }
+  turn = "Two";
+  highlightPlayerTurn();
+  render();
+}
+
+// determines player 2 win or tie ; switches turn; highlights active card
+function takeTurnTwo() {
+  if (turn === "One" || winner) return;
+  playerTwoPlayed = playerTwoDeck.shift();
+  if (cardsAreEqual()) {
+    playerTwoWinEl.innerText = "Player 2 Wins !";
+    winner = "Two";
+  }
+  if (playerTwoDeck.length === 0) {
+    winner = "Tie";
+  }
+  turn = "One";
+  highlightPlayerTurn();
+  render();
+}
+// displays the  Active card by adding classlist and text from the card in the deck ; decides if tie
+function render() {
+  activeCardOneEl.className = "card xlarge";
+  activeCardTwoEl.className = "card xlarge";
+
+  if (playerOnePlayed) {
+    activeCardOneEl.classList.add(playerOnePlayed);
+    activeCardOneEl.textContent = playerOnePlayed;
+  }
+
+  if (playerTwoPlayed) {
+    activeCardTwoEl.classList.add(playerTwoPlayed);
+    activeCardTwoEl.textContent = playerTwoPlayed;
+  }
+
+  if (winner) {
+    if (winner === "Tie") {
+      itsATieEl.innerText = "Its a tie !";
+    } else {
+      shuffleBtn.style.backgroundColor = "red";
+    }
+  }
+}
